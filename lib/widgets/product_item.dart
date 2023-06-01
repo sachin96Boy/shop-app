a879443be4433cf04f5260e14984210f9d7f8590
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-06-01 12:09:30 +0530

    chor: using consumer to rebuilds parts on the widget tree

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,37 +8,39 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: GridTile(
-        footer: GridTileBar(
-            backgroundColor: Colors.black.withOpacity(0.6),
-            leading: IconButton(
-              icon: Icon(!product.isFavourite
-                  ? Icons.favorite_border
-                  : Icons.favorite),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {
-                product.toggleFavourite();
-              },
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(
+      builder: (context, product, _) => ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: GridTile(
+          footer: GridTileBar(
+              backgroundColor: Colors.black.withOpacity(0.6),
+              leading: IconButton(
+                icon: Icon(!product.isFavourite
+                    ? Icons.favorite_border
+                    : Icons.favorite),
+                color: Theme.of(context).colorScheme.secondary,
+                onPressed: () {
+                  product.toggleFavourite();
+                },
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.shopping_bag),
+                color: Theme.of(context).colorScheme.secondary,
+                onPressed: () {},
+              ),
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              )),
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: {'id': product.id}),
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
-            trailing: IconButton(
-              icon: const Icon(Icons.shopping_bag),
-              color: Theme.of(context).colorScheme.secondary,
-              onPressed: () {},
-            ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            )),
-        child: GestureDetector(
-          onTap: () => Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: {'id': product.id}),
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
           ),
         ),
       ),
