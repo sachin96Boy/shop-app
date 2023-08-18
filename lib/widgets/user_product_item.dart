a775017a80371ffcb674e8f37c18e4ecf02e1daa
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-08-18 22:35:09 +0530

    chor: delete items

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product_provider.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -6,7 +8,10 @@ class UserProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
   const UserProductItem(
-      {super.key, required this.title, required this.imageUrl, required this.id});
+      {super.key,
+      required this.title,
+      required this.imageUrl,
+      required this.id});
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +23,17 @@ class UserProductItem extends StatelessWidget {
         child: Row(children: [
           IconButton(
             onPressed: () {
-              Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              Navigator.of(context)
+                  .pushNamed(EditProductScreen.routeName, arguments: id);
             },
             icon: const Icon(Icons.edit),
             color: Theme.of(context).colorScheme.primary,
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<ProductsProvider>(context, listen: false)
+                  .deleteProduct(id);
+            },
             icon: const Icon(Icons.delete),
             color: Theme.of(context).colorScheme.error,
           )
